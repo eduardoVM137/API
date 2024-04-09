739842ccca0058db5fa4d39c7284dc3cd3acc464
eduardoVM137 <blodcplay@hotmail.com>
2024-04-09 10:14:20 -0500

    yaml

---
+++ Parcial 2/yaml/index.js
@@ -0,0 +1,9 @@
+import YAML from 'yaml'
+import fs from 'fs'
+
+const file = fs.readFileSync('./Parcial 2/yaml/afile.yml', 'utf8');
+
+YAML.parse(file)
+console.log("patas")
+console.log(
+    YAML.parse(file))
