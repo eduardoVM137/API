ae206958c482edac89855e66185c4d82aa2c3a8e
eduardoVM137 <blodcplay@hotmail.com>
2024-04-15 00:28:31 -0500

    servidor no seguro

--- Parcial 2/Servidor Seguro/index.js
+++ Parcial 2/Servidor Seguro/index.js
@@ -1,22 +1,58 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
+import https from 'https';
+import { fileURLToPath } from 'url';
 
-import fs from 'fs'
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
+const keyPath = path.join(__dirname, 'Certificado', 'key.pem');
+const certPath = path.join(__dirname, 'Certificado', 'cert.pem');
 
-
-
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-});
 const options = {
-    key: fs.readFileSync(express.static(path.join(__dirname, './Certificado/key.pem'))),
-    cert: fs.readFileSync(express.static(path.join(__dirname, './Certificado/cert.pem')))
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
 };
 
-https.createServer(options, (req, res) => {
-    res.writeHead(200);
-    res.end('hello world\n');
-}).listen(8000);
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+https.createServer(options, app).listen(8000, () => {
+    console.log('Server is running on https://localhost:8000');
+});
+
+// import express from 'express';
+// import path from 'path';
+// import fs from 'fs'
+
+// import https from 'https';
+
+// import { fileURLToPath } from 'url';
+// const app = express();
+
+
+// const __filename = fileURLToPath(
+//     import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const keyPath = path.join(__dirname, 'Certificado', 'key.pem');
+// const certPath = path.join(__dirname, 'Certificado', 'cert.pem');
+
+// app.get('/', (req, res, next) => {
+//     res.send('Hello World!');
+// });
+// const options = {
+//     key: fs.readFileSync(keyPath),
+//     cert: fs.readFileSync(certPath)
+// };
+
+// https.createServer(options, (req, res) => {
+//     res.writeHead(200);
+//     res.end('hello world\n');
+// }).listen(8000);lo world\n');
+// }).listen(8000);
